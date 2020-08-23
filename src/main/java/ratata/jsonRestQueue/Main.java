f9cd3c6e24d5a395f345916e2d9dc47c88858d66
EHD1HC <EHD1HC@host.docker.internal>
2020-08-23 14:42:30 +0700

    add quit persist data

--- src/main/java/ratata/jsonRestQueue/Main.java
+++ src/main/java/ratata/jsonRestQueue/Main.java
@@ -1,5 +1,10 @@
 package ratata.jsonRestQueue;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -20,15 +25,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Main {
 
 	private static ObjectMapper mapper = new ObjectMapper();
+	private static String queueName = "RestQueue";
 	private static String host;
 	private static int port;
 	private static int queueSize = 100;
 	private static boolean stop = false;
-	private static final Map<String, ArrayBlockingQueue<JsonNode>> restQueue = new HashMap<String, ArrayBlockingQueue<JsonNode>>();
+	private static Map<String, ArrayBlockingQueue<JsonNode>> restQueue = new HashMap<String, ArrayBlockingQueue<JsonNode>>();
 
 	@GET("/*")
 	public Object download(Req req) {
 		try {
+			if (stop) {
+				throw new Exception("stoping");
+			}
 			return restQueue.get(req.uri()).poll();
 		} catch (Exception e) {
 			req.response().code(500);
@@ -83,14 +92,9 @@ public class Main {
 	}
 
 	@PUT("/quit")
-	public void quit() throws InterruptedException {
+	public void quit() throws Exception {
 		stop = true;
-		for (String key : restQueue.keySet()) {
-			ArrayBlockingQueue<JsonNode> queue = restQueue.get(key);
-			while (!queue.isEmpty()) {
-				Thread.sleep(1000);
-			}
-		}
+		saveQueue();
 		System.exit(0);
 	}
 
@@ -99,17 +103,57 @@ public class Main {
 		System.exit(0);
 	}
 
-	public static void main(String[] args) {
+	public static void saveQueue() throws Exception {
+		FileOutputStream fos = new FileOutputStream(queueName);
+		PrintWriter writer = new PrintWriter(fos);
+		writer.println(queueSize);
+		for (String key : restQueue.keySet()) {
+			ArrayBlockingQueue<JsonNode> queue = restQueue.get(key);
+			writer.println("#".concat(key));
+			while (!queue.isEmpty()) {
+				JsonNode data = queue.poll();
+				writer.println(mapper.writeValueAsString(data));
+			}
+		}
+		writer.flush();
+		fos.flush();
+		fos.close();
+	}
+
+	public static void loadQueue() throws Exception {
+		BufferedReader reader = new BufferedReader(new FileReader(queueName));
+		String key = null;
+		String line = reader.readLine();
+		queueSize = Integer.valueOf(line).intValue();
+		while (true) {
+			line = reader.readLine();
+			if (line == null) {
+				break;
+			}
+			if (line.startsWith("#")) {
+				key = line.substring(1);
+				restQueue.put(key, new ArrayBlockingQueue<JsonNode>(queueSize));
+				continue;
+			}
+			restQueue.get(key).put(mapper.readTree(line));
+		}
+		reader.close();
+	}
+
+	public static void main(String[] args) throws Exception {
 		App.boot();
 		if (args.length > 0) {
-			host = args[0];
-			port = Integer.valueOf(args[1]).intValue();
-			queueSize = Integer.valueOf(args[2]).intValue();
-			Setup.create("RestQueue").address(host).port(port).scan();
+			queueName = args[0];
+			host = args[1];
+			port = Integer.valueOf(args[2]).intValue();
+			queueSize = Integer.valueOf(args[3]).intValue();
+			Setup.create(queueName).address(host).port(port).scan();
 		} else {
-			Setup.create("RestQueue").address("0.0.0.0").port(9090).scan();
+			Setup.create(queueName).address("0.0.0.0").port(9090).scan();
 		}
-
+		if ((new File(queueName)).exists()) {
+			loadQueue();
+		}
 	}
 
 }
